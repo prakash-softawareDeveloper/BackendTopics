6d72b8e98d5006a3f34f1efa94342000a767739d
prakash-4u <prakash96m@gmail.com>
2024-06-26 22:48:47 +0530

    getting values from HTTp

--- src/main/java/com/demo1/productservices/controller/SampleController.java
+++ src/main/java/com/demo1/productservices/controller/SampleController.java
@@ -10,13 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/say")
 public class SampleController {
 
-    @GetMapping("/hello/{name}/{times}")
-  public String sayHello(@PathVariable("name") String name, @PathVariable("times") int times) {
-        String output="";
-     for(int i=0; i<times; i++) {
-        output = output + "Hello "+ name +" ";
-     }
-     return output;
+    @GetMapping("/hello/{name}")
+  public String sayHello(@PathVariable("name") String name) {
+
+     return "Hello " + name;
   }
 
   @GetMapping("/bye")
