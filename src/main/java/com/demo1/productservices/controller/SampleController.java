b623a5ba0ad25f158544c29c40cb77e9f668f135
prakash-4u <prakash96m@gmail.com>
2024-06-26 22:29:40 +0530

    read values from URL

--- src/main/java/com/demo1/productservices/controller/SampleController.java
+++ src/main/java/com/demo1/productservices/controller/SampleController.java
@@ -2,6 +2,7 @@ package com.demo1.productservices.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/say")
 public class SampleController {
 
-    @GetMapping("/hello")
-  public String sayHello() {
-      return "Hello Everyone";
+    @GetMapping("/hello/{name}")
+  public String sayHello(@PathVariable("name") String name) {
+      return "Hello "+name;
   }
 
   @GetMapping("/bye")
